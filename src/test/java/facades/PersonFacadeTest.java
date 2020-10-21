3477028799db4a9b0b04c9f5b4f095f7d16746ff
Mathias <maddy1617@hotmail.com>
2020-10-21 16:07:17 +0200

    lidt test

--- src/test/java/facades/PersonFacadeTest.java
+++ src/test/java/facades/PersonFacadeTest.java
@@ -1,18 +1,31 @@
 package facades;
 
+import dto.PersonDTO;
+import dto.PersonsDTO;
+import entities.Address;
+import entities.CityInfo;
+import entities.Hobby;
 import entities.Person;
+import entities.Phone;
+import java.util.ArrayList;
+import javax.persistence.EntityManager;
 import utils.EMF_Creator;
 import javax.persistence.EntityManagerFactory;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.hamcrest.Matchers;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 //Uncomment the line below, to temporarily disable this test
 //@Disabled
 public class PersonFacadeTest {
-    private static Person p1,p2,p3,p4,p5;
+    private static Person p1,p2,p3;
     private static EntityManagerFactory emf;
     private static PersonFacade facade;
 
@@ -32,26 +45,56 @@ public class PersonFacadeTest {
 
     @BeforeEach
     public void setUp() {
+    EntityManager em = emf.createEntityManager();
     
-//        EntityManager em = emf.createEntityManager();
-//        Date date = new Date();
-//        c1 = new Cars(1997, "Ford", "E350", 3000, "test", date);
-//        c2 = new Cars(1999, "Chevy", "Venture", 4900, "test1", date);
-//        c3 = new Cars(2000, "Chevy", "Venture", 5000, "test3", date);
-//        c4 = new Cars(1996, "Jeep", "Grand Cherokee", 4799, "test4", date);
-//        c5 = new Cars(2005, "Volvo", "v70", 44799, "test5", date);
-//                try {
-//            em.getTransaction().begin();
-//            em.createQuery("DELETE from Cars").executeUpdate();
-//            em.persist(c1);
-//            em.persist(c2);
-//            em.persist(c3);
-//            em.persist(c4);
-//            em.persist(c5);
-//            em.getTransaction().commit();
-//        } finally {
-//            em.close();
-//        }
+     p1 = new Person("eko", "ekolastname", "eko@mail.com");
+     p2 = new Person("eko1", "ekolastname1", "eko1@mail.com");
+     p3 = new Person("eko2", "ekolastname2", "eko2@mail.com");
+    Phone ph1 = new Phone("12345678");
+    Phone ph2 = new Phone("23456789");
+    Phone ph3 = new Phone("34567890");
+    Hobby h1 = new Hobby("skiløb", "wikilink", "category", "type");
+    Hobby h2 = new Hobby("tennis", "wikilink1", "category1", "type1");
+    Hobby h3 = new Hobby("fodbold", "wikilink2", "category2", "type2");
+    Address a1 = new Address("ekostreet");
+    Address a2 = new Address("ekostreet2");
+    Address a3 = new Address("ekostreet3");
+    CityInfo c1 = new CityInfo("1234", "Lyngby");
+    CityInfo c2 = new CityInfo("4321", "Gentofte");
+    CityInfo c3 = new CityInfo("2345", "København");
+    
+    ph1.setPerson(p1);
+    ph2.setPerson(p2);
+    ph3.setPerson(p3);
+    
+    a1.setCityInfo(c3);
+    a2.setCityInfo(c2);
+    a3.setCityInfo(c1);
+    
+    p1.setAddress(a1);
+    p2.setAddress(a2);
+    p3.setAddress(a3);
+    
+    p1.addHobby(h3);
+    p2.addHobby(h2);
+    p3.addHobby(h1);
+    
+    try {
+            em.getTransaction().begin();
+            em.createQuery("DELETE from Phone").executeUpdate();
+            em.createQuery("DELETE from Person").executeUpdate();
+            em.createQuery("DELETE from Address").executeUpdate();
+            em.createQuery("DELETE from CityInfo").executeUpdate();    
+            em.createQuery("DELETE from Hobby").executeUpdate();
+            
+            em.persist(p1);
+            em.persist(p2);
+            em.persist(p3);
+
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
     }
 
     @AfterEach
@@ -59,15 +102,31 @@ public class PersonFacadeTest {
 
     }
 
-   
-//    @Test
-//    public void testCarCount() {
-//        assertEquals(5, facade.getPersonCount());
-//    }
-    
+    @Disabled
     @Test
-    public void testAllCars() {
-      //  assertEquals(members, facade.getAllNames());
+    public void testPersonCount() {
+        assertEquals(3, facade.getPersonCount());
+    }
+    @Disabled  
+    @Test
+    public void testAllPersons() {
+
+//        PersonDTO p1DTO = new PersonDTO(p1);
+//        PersonDTO p2DTO = new PersonDTO(p2);
+//        PersonDTO p3DTO = new PersonDTO(p3);
+//        assertEquals(facade.getAllPersons(), containsInAnyOrder(p1DTO, p2DTO, p3DTO));
+        
+        
+        
+        
+        
+        
+//        ArrayList<Person> people = new ArrayList<>();
+//        people.add(p1);
+//        people.add(p2);
+//        people.add(p3);
+//        
+//        assertEquals(facade.getAllPersons(), Matchers.containsInAnyOrder(people));
     }
     
     
